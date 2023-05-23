1d9a8a20a21500d192fd33bf5bc0eb93e721007e
Andy Newton <andy.newton@icann.org>
2023-05-23 13:38:02 -0400

    serializing postal address

--- icann-rdap-common/src/contact/to_vcard.rs
+++ icann-rdap-common/src/contact/to_vcard.rs
@@ -78,6 +78,77 @@ impl Contact {
             }
         }
 
+        if let Some(phones) = &self.phones {
+            for phone in phones {
+                let mut params: Map<String, Value> = Map::new();
+                if let Some(pref) = phone.preference {
+                    params.insert("pref".to_string(), Value::from(pref));
+                }
+                let mut types: Vec<String> = Vec::new();
+                if let Some(contexts) = &phone.contexts {
+                    types.append(&mut contexts.clone());
+                }
+                if let Some(features) = &phone.features {
+                    types.append(&mut features.clone());
+                }
+                params.insert("type".to_string(), vec_string_to_param(&types));
+                vcard.push(json!(["tel", Value::from(params), "text", phone.phone]))
+            }
+        }
+
+        if let Some(addrs) = &self.postal_addresses {
+            for addr in addrs {
+                let mut params: Map<String, Value> = Map::new();
+                if let Some(pref) = addr.preference {
+                    params.insert("pref".to_string(), Value::from(pref));
+                }
+                if let Some(contexts) = addr.contexts.as_ref() {
+                    params.insert("type".to_string(), vec_string_to_param(contexts));
+                }
+                if let Some(full_address) = &addr.full_address {
+                    params.insert(
+                        "label".to_string(),
+                        Value::from_str(full_address).expect("serializing full address"),
+                    );
+                }
+                let mut lines: Vec<String> = Vec::new();
+                if let Some(street_parts) = &addr.street_parts {
+                    lines.push(street_parts.get(0).cloned().unwrap_or("".to_string()));
+                    lines.push(street_parts.get(1).cloned().unwrap_or("".to_string()));
+                    lines.push(street_parts.get(2).cloned().unwrap_or("".to_string()));
+                } else {
+                    lines.push("".to_string());
+                    lines.push("".to_string());
+                    lines.push("".to_string());
+                }
+                if let Some(locality) = &addr.locality {
+                    lines.push(locality.to_owned());
+                } else {
+                    lines.push("".to_string());
+                }
+                if let Some(region_name) = &addr.region_name {
+                    lines.push(region_name.to_owned());
+                } else if let Some(region_code) = &addr.region_code {
+                    lines.push(region_code.to_owned());
+                } else {
+                    lines.push("".to_string());
+                }
+                if let Some(postal_code) = &addr.postal_code {
+                    lines.push(postal_code.to_owned());
+                } else {
+                    lines.push("".to_string());
+                }
+                if let Some(country_name) = &addr.country_name {
+                    lines.push(country_name.to_owned());
+                } else if let Some(country_code) = &addr.country_code {
+                    lines.push(country_code.to_owned());
+                } else {
+                    lines.push("".to_string());
+                }
+                vcard.push(json!(["adr", Value::from(params), "text", lines]))
+            }
+        }
+
         // return the vcard array
         vec![
             Value::from_str("vcard").expect("unable to create vcard literal"),
