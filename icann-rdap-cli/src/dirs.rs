67e5b4bfc5de021f726636691c43ffa3dfa34846
Andrew Newton <andy@hxr.us>
2023-06-23 18:40:33 -0400

    infrastructure for cache directories

--- icann-rdap-cli/src/dirs.rs
+++ icann-rdap-cli/src/dirs.rs
@@ -13,6 +13,8 @@ pub(crate) const ORGANIZATION: &str = "ICANN";
 pub(crate) const APPLICATION: &str = "rdap";
 
 pub(crate) const ENV_FILE_NAME: &str = "rdap.env";
+pub(crate) const RDAP_CACHE_NAME: &str = "rdap_cache";
+pub(crate) const BOOTSTRAP_CACHE_NAME: &str = "bootstrap_cache";
 
 lazy_static! {
     pub(crate) static ref PROJECT_DIRS: ProjectDirs =
@@ -24,6 +26,10 @@ lazy_static! {
 pub(crate) fn init() -> Result<(), CliError> {
     create_dir_all(PROJECT_DIRS.config_dir())?;
     create_dir_all(PROJECT_DIRS.cache_dir())?;
+    create_dir_all(rdap_cache_path())?;
+    create_dir_all(bootstrap_cache_path())?;
+
+    // create default config file
     if !config_path().exists() {
         let example_config = include_str!("rdap.env");
         write(config_path(), example_config)?;
@@ -38,6 +44,17 @@ pub(crate) fn reset() -> Result<(), CliError> {
     init()
 }
 
+/// Returns a [PathBuf] to the configuration file.
 pub(crate) fn config_path() -> PathBuf {
     PROJECT_DIRS.config_dir().join(ENV_FILE_NAME)
+}
+
+/// Returns a [PathBuf] to the cache directory for RDAP responses.
+pub(crate) fn rdap_cache_path() -> PathBuf {
+    PROJECT_DIRS.cache_dir().join(RDAP_CACHE_NAME)
+}
+
+/// Returns a [PathBuf] to the cache directory for bootstrap files.
+pub(crate) fn bootstrap_cache_path() -> PathBuf {
+    PROJECT_DIRS.cache_dir().join(BOOTSTRAP_CACHE_NAME)
 }
