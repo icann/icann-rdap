acb015037efd16efaf9cfa60828d64cc7e5ef92e
Andy Newton <andy@hxr.us>
2025-02-08 13:42:30 -0500

    add: documentation

--- icann-rdap-common/src/media_types.rs
+++ icann-rdap-common/src/media_types.rs
@@ -1,4 +1,7 @@
 //! RDAP media types (formerly known as mime types).
 
+/// The "application/json" media type value.
 pub const JSON_MEDIA_TYPE: &str = "application/json";
+
+/// The "application/rdap+json" media type value.
 pub const RDAP_MEDIA_TYPE: &str = "application/rdap+json";
