9d5fbe85c5895b6cc26b88ca688fb7832c286203
Andy Newton <andy@hxr.us>
2025-08-18 08:45:40 -0400

    change: help getter and builder docs

--- icann-rdap-common/src/response/help.rs
+++ icann-rdap-common/src/response/help.rs
@@ -7,6 +7,17 @@ use {
 use super::{to_opt_vec, Common, CommonFields, ToResponse};
 
 /// Represents an RDAP help response.
+///
+/// Use the builders to create one:
+/// ```rust
+/// use icann_rdap_common::prelude::*;
+///
+/// let e = Help::builder()
+///   .build();
+/// ```
+///
+/// Use the getter functions to access information.
+/// See [CommonFields] for common getter functions.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
 pub struct Help {
     #[serde(flatten)]
