0bb13b8d50597f3d68846bd1407eb59000af4e26
Andy newton <andy@hxr.us>
2025-11-11 17:27:48 -0500

    chore:figuring out the borrowing

--- icann-rdap-client/src/rdap/redacted/simplify.rs
+++ icann-rdap-client/src/rdap/redacted/simplify.rs
@@ -1,6 +1,6 @@
 //! Changes RFC 9537 redactions to simple redactions
 
-use icann_rdap_common::prelude::RdapResponse;
+use icann_rdap_common::prelude::{RdapResponse, ToResponse};
 
 /// Takes in an RDAP response and creates Simple Redactions
 /// from the RFC 9537 redactions.
@@ -11,17 +11,30 @@ use icann_rdap_common::prelude::RdapResponse;
 /// * `only_pre_path` does not create Simple Redactions if no path expression is given or the prePath expression is present.
 pub fn simplify_redactions(rdap: RdapResponse, _only_pre_path: bool) -> RdapResponse {
     match rdap {
-        RdapResponse::Entity(_entity) => todo!(),
-        RdapResponse::Domain(_domain) => todo!(),
-        RdapResponse::Nameserver(_nameserver) => todo!(),
-        RdapResponse::Autnum(_autnum) => todo!(),
-        RdapResponse::Network(_network) => todo!(),
+        RdapResponse::Entity(entity) => {
+            // no registered redactions are on plain entities. They must all
+            // have roles.
+            entity.to_response()
+        }
+        RdapResponse::Domain(domain) => domain.to_response(),
+        RdapResponse::Nameserver(nameserver) => {
+            // no registered redactions on nameservers.
+            nameserver.to_response()
+        }
+        RdapResponse::Autnum(autnum) => {
+            // no registered redactions on autnums.
+            autnum.to_response()
+        }
+        RdapResponse::Network(network) => {
+            // no registered redactons on networks
+            network.to_response()
+        }
         _ => {
             // do nothing as RFC 9537 does not explain how or if its redacted
             // directives work against search results or other, non-object class responses.
+            rdap
         }
     }
-    rdap
 }
 
 /// Removes RFC 9537 redactions from an RDAP response.
