152edea406b9b5b8d03547ffec5a923710240396
Andy newton <andy@hxr.us>
2026-02-14 12:36:21 -0500

    chore: fix clippy lint

--- icann-rdap-client/src/md/network.rs
+++ icann-rdap-client/src/md/network.rs
@@ -67,11 +67,13 @@ impl ToMd for Network {
 
 impl MdUtil for Network {
     fn get_header_text(&self) -> MdHeaderText {
-        let header_text = if self.start_address.is_some() && self.end_address.is_some() {
+        let header_text = if let (Some(start_address), Some(end_address)) =
+            (&self.start_address, &self.end_address)
+        {
             format!(
                 "IP Network {} - {}",
-                &self.start_address.as_ref().unwrap().replace_md_chars(),
-                &self.end_address.as_ref().unwrap().replace_md_chars()
+                start_address.replace_md_chars(),
+                end_address.replace_md_chars()
             )
         } else if let Some(start_address) = &self.start_address {
             format!("IP Network {}", start_address.replace_md_chars())
