2742a6929a119818114df37ed6d80e51c5057c01
Andy newton <andy@hxr.us>
2026-02-14 12:26:31 -0500

    chore: fix lint on unwrap after using is_some

--- icann-rdap-client/src/md/autnum.rs
+++ icann-rdap-client/src/md/autnum.rs
@@ -71,11 +71,13 @@ impl ToMd for Autnum {
 
 impl MdUtil for Autnum {
     fn get_header_text(&self) -> MdHeaderText {
-        let header_text = if self.start_autnum.is_some() && self.end_autnum.is_some() {
+        let header_text = if let (Some(start_autnum), Some(end_autnum)) =
+            (&self.start_autnum, &self.end_autnum)
+        {
             format!(
                 "Autonomous Systems {} - {}",
-                &self.start_autnum.as_ref().unwrap().replace_md_chars(),
-                &self.end_autnum.as_ref().unwrap().replace_md_chars()
+                start_autnum.replace_md_chars(),
+                &end_autnum.replace_md_chars()
             )
         } else if let Some(start_autnum) = &self.start_autnum {
             format!("Autonomous System {}", start_autnum.replace_md_chars())
