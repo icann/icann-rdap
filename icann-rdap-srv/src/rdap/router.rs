975b1218a54cd45fe309999c8066768bb98a7b60
Andy newton <andy@hxr.us>
2026-04-12 09:29:25 -0400

    feat(srv): handlers for rdns rdap-up, rdap-down, rdap-top, rdap-bottom

--- icann-rdap-srv/src/rdap/router.rs
+++ icann-rdap-srv/src/rdap/router.rs
@@ -4,7 +4,7 @@ use super::{
     autnum::autnum_by_num,
     autnums::{autnum_rdap_bottom, autnum_rdap_down, autnum_rdap_top, autnum_rdap_up},
     domain::domain_by_name,
-    domains::domains,
+    domains::{domain_rdap_bottom, domain_rdap_down, domain_rdap_top, domain_rdap_up, domains},
     entities::entities,
     entity::entity_by_handle,
     ip::network_by_netid,
@@ -35,6 +35,16 @@ pub(crate) fn rdap_router() -> Router<crate::server::DynServiceState> {
         .route("/nameserver/:name", get(nameserver_by_name))
         .route("/entity/:handle", get(entity_by_handle))
         .route("/domains", get(domains))
+        .route("/domains/rirSearch1/rdap-up/*domain", get(domain_rdap_up))
+        .route("/domains/rirSearch1/rdap-top/*domain", get(domain_rdap_top))
+        .route(
+            "/domains/rirSearch1/rdap-down/*domain",
+            get(domain_rdap_down),
+        )
+        .route(
+            "/domains/rirSearch1/rdap-bottom/*domain",
+            get(domain_rdap_bottom),
+        )
         .route("/nameservers", get(nameservers))
         .route("/entities", get(entities))
         .route("/help", get(srvhelp))
