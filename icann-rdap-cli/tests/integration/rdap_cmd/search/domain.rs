1963b18744388a58c8cb6a56c357a9b20750ad4a
Andy newton <andy@hxr.us>
2026-04-28 06:21:07 -0400

    refactor(cli): move domain search out of entity.rs to domain.rs

--- icann-rdap-cli/tests/integration/rdap_cmd/search/domain.rs
+++ icann-rdap-cli/tests/integration/rdap_cmd/search/domain.rs
@@ -1,4 +1,7 @@
-use {icann_rdap_common::response::Domain, icann_rdap_srv::storage::StoreOps};
+use {
+    icann_rdap_common::{response::Domain, response::Nameserver},
+    icann_rdap_srv::storage::StoreOps,
+};
 
 use crate::test_jig::TestJig;
 
@@ -14,6 +17,32 @@ async fn test_domain_search() {
 
     // WHEN search for the domain
     test_jig.cmd.arg("-t").arg("domain-name").arg("foo.*");
+
+    // THEN success
+    let assert = test_jig.cmd.assert();
+    assert.success();
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_domain_search_by_nameserver_ldh_name() {
+    // GIVEN domain with nameserver
+    let mut test_jig = TestJig::new_rdap_with_search().await;
+    let mut tx = test_jig.mem.new_tx().await.expect("new transaction");
+    tx.add_domain(
+        &Domain::builder()
+            .ldh_name("foo.example")
+            .nameservers(vec![Nameserver::builder()
+                .ldh_name("ns.foo.example")
+                .build()
+                .unwrap()])
+            .build(),
+    )
+    .await
+    .expect("add domain in tx");
+    tx.commit().await.expect("tx commit");
+
+    // WHEN search for domains by nameserver ldhName
+    test_jig.cmd.arg("-t").arg("domain-ns-name").arg("ns.foo.*");
 
     // THEN success
     let assert = test_jig.cmd.assert();
--- icann-rdap-cli/tests/integration/rdap_cmd/search/entity.rs
+++ icann-rdap-cli/tests/integration/rdap_cmd/search/entity.rs
@@ -1,8 +1,5 @@
 use {
-    icann_rdap_common::{
-        contact::Contact,
-        response::{Domain, Entity, Nameserver},
-    },
+    icann_rdap_common::{contact::Contact, response::Entity},
     icann_rdap_srv::storage::StoreOps,
 };
 
@@ -49,28 +46,4 @@ async fn test_entity_name_search() {
     assert.success();
 }
 
-#[tokio::test(flavor = "multi_thread")]
-async fn test_domain_search_by_nameserver_ldh_name() {
-    // GIVEN domain with nameserver
-    let mut test_jig = TestJig::new_rdap_with_search().await;
-    let mut tx = test_jig.mem.new_tx().await.expect("new transaction");
-    tx.add_domain(
-        &Domain::builder()
-            .ldh_name("foo.example")
-            .nameservers(vec![Nameserver::builder()
-                .ldh_name("ns.foo.example")
-                .build()
-                .unwrap()])
-            .build(),
-    )
-    .await
-    .expect("add domain in tx");
-    tx.commit().await.expect("tx commit");
 
-    // WHEN search for domains by nameserver ldhName
-    test_jig.cmd.arg("-t").arg("domain-ns-name").arg("ns.foo.*");
-
-    // THEN success
-    let assert = test_jig.cmd.assert();
-    assert.success();
-}
