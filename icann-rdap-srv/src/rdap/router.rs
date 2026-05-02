a4c9ae5e2ee560cebfde9c3657c5a9da307b4146
Andy newton <andy@hxr.us>
2026-05-02 10:18:51 -0400

    chore: upgrade axum and friends

--- icann-rdap-srv/src/rdap/router.rs
+++ icann-rdap-srv/src/rdap/router.rs
@@ -16,45 +16,51 @@ use super::{
 
 pub(crate) fn rdap_router() -> Router<crate::server::DynServiceState> {
     Router::new()
-        .route("/domain/:domain", get(domain_by_name))
+        .route("/domain/{domain}", get(domain_by_name))
         .route("/ips", get(networks))
-        .route("/ip/*netid", get(network_by_netid))
-        .route("/autnum/:asnumber", get(autnum_by_num))
-        .route("/autnums/rirSearch1/rdap-up/*asNumber", get(autnum_rdap_up))
+        .route("/ip/{*netid}", get(network_by_netid))
+        .route("/autnum/{asnumber}", get(autnum_by_num))
         .route(
-            "/autnums/rirSearch1/rdap-top/*asNumber",
+            "/autnums/rirSearch1/rdap-up/{*asNumber}",
+            get(autnum_rdap_up),
+        )
+        .route(
+            "/autnums/rirSearch1/rdap-top/{*asNumber}",
             get(autnum_rdap_top),
         )
         .route(
-            "/autnums/rirSearch1/rdap-down/*asNumber",
+            "/autnums/rirSearch1/rdap-down/{*asNumber}",
             get(autnum_rdap_down),
         )
         .route(
-            "/autnums/rirSearch1/rdap-bottom/*asNumber",
+            "/autnums/rirSearch1/rdap-bottom/{*asNumber}",
             get(autnum_rdap_bottom),
         )
-        .route("/nameserver/:name", get(nameserver_by_name))
+        .route("/nameserver/{name}", get(nameserver_by_name))
         .route("/entities", get(entities))
-        .route("/entity/:handle", get(entity_by_handle))
+        .route("/entity/{handle}", get(entity_by_handle))
         .route("/autnums", get(autnums))
         .route("/domains", get(domains))
-        .route("/domains/rirSearch1/rdap-up/*domain", get(domain_rdap_up))
-        .route("/domains/rirSearch1/rdap-top/*domain", get(domain_rdap_top))
+        .route("/domains/rirSearch1/rdap-up/{*domain}", get(domain_rdap_up))
         .route(
-            "/domains/rirSearch1/rdap-down/*domain",
+            "/domains/rirSearch1/rdap-top/{*domain}",
+            get(domain_rdap_top),
+        )
+        .route(
+            "/domains/rirSearch1/rdap-down/{*domain}",
             get(domain_rdap_down),
         )
         .route(
-            "/domains/rirSearch1/rdap-bottom/*domain",
+            "/domains/rirSearch1/rdap-bottom/{*domain}",
             get(domain_rdap_bottom),
         )
         .route("/nameservers", get(nameservers))
         .route("/help", get(srvhelp))
-        .route("/ips/rirSearch1/rdap-up/*ipAddress", get(ip_rdap_up))
-        .route("/ips/rirSearch1/rdap-top/*ipAddress", get(ip_rdap_top))
-        .route("/ips/rirSearch1/rdap-down/*ipAddress", get(ip_rdap_down))
+        .route("/ips/rirSearch1/rdap-up/{*ipAddress}", get(ip_rdap_up))
+        .route("/ips/rirSearch1/rdap-top/{*ipAddress}", get(ip_rdap_top))
+        .route("/ips/rirSearch1/rdap-down/{*ipAddress}", get(ip_rdap_down))
         .route(
-            "/ips/rirSearch1/rdap-bottom/*ipAddress",
+            "/ips/rirSearch1/rdap-bottom/{*ipAddress}",
             get(ip_rdap_bottom),
         )
 }
