18265276e97b51286dd22f765c9ed9702e93759f
Andy newton <andy@hxr.us>
2026-08-19 12:45:40 -0400

    chore(srv): first working version of migrate

--- icann-rdap-srv/migrations/001_init.sql
+++ icann-rdap-srv/migrations/001_init.sql
@@ -1,34 +1,41 @@
 CREATE TABLE entity (
-    handle  TEXT PRIMARY KEY,
+    id      TEXT PRIMARY KEY, -- application generated
+    handle  TEXT GENERATED ALWAYS AS (content->>'handle') STORED,
     content JSONB NOT NULL
 );
 
 CREATE TABLE domain (
-    ldh_name     TEXT PRIMARY KEY,
-    unicode_name TEXT,
-    handle       TEXT,
+    ldh_name     TEXT GENERATED ALWAYS AS (content->>'ldhName') STORED PRIMARY KEY,
+    unicode_name TEXT GENERATED ALWAYS AS (content->>'unicodeName') STORED,
+    handle       TEXT GENERATED ALWAYS AS (content->>'handle') STORED,
     content      JSONB NOT NULL
 );
+
+CREATE UNIQUE INDEX domain_ldh_name_lower_idx ON domain (LOWER(ldh_name));
 
 CREATE TABLE nameserver (
-    ldh_name     TEXT PRIMARY KEY,
-    unicode_name TEXT,
-    handle       TEXT,
+    ldh_name     TEXT GENERATED ALWAYS AS (content->>'ldhName') STORED PRIMARY KEY,
+    unicode_name TEXT GENERATED ALWAYS AS (content->>'unicodeName') STORED,
+    handle       TEXT GENERATED ALWAYS AS (content->>'handle') STORED,
     content      JSONB NOT NULL
 );
 
+CREATE UNIQUE INDEX nameserver_ldh_name_lower_idx ON nameserver (LOWER(ldh_name));
+
 CREATE TABLE autnum (
-    start_autnum BIGINT PRIMARY KEY,
-    end_autnum   BIGINT NOT NULL,
-    handle       TEXT,
-    content      JSONB NOT NULL
+    start_autnum BIGINT GENERATED ALWAYS AS ((content->>'startAutnum')::bigint) STORED NOT NULL,
+    end_autnum   BIGINT GENERATED ALWAYS AS ((content->>'endAutnum')::bigint) STORED NOT NULL,
+    handle       TEXT GENERATED ALWAYS AS (content->>'handle') STORED,
+    content      JSONB NOT NULL,
+    PRIMARY KEY (start_autnum, end_autnum)
 );
 
 CREATE TABLE network (
-    start_address INET PRIMARY KEY,
-    end_address   INET NOT NULL,
-    handle        TEXT,
-    content       JSONB NOT NULL
+    start_address INET GENERATED ALWAYS AS ((content->>'startAddress')::inet) STORED NOT NULL,
+    end_address   INET GENERATED ALWAYS AS ((content->>'endAddress')::inet) STORED NOT NULL,
+    handle        TEXT GENERATED ALWAYS AS (content->>'handle') STORED,
+    content       JSONB NOT NULL,
+    PRIMARY KEY (start_address, end_address)
 );
 
 CREATE TABLE srv_help (
